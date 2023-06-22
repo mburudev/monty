26b1fcccbdfb06a558c10df5d518e5d8e1482df9
mburudev <ianmburu999@gmail.com>
2023-06-22 12:30:25 -0700

    Task 0 attempt 10'

--- push.c
+++ push.c
@@ -1,35 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define STACK_SIZE 100
+
+int stack[STACK_SIZE];
+int top = -1;
+
 /**
- * main - Entry point of the program
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
+ * push - Pushes an element onto the stack
+ * @num: The integer value to be pushed
  *
- * Return: 0 on success, 1 on failure
+ * Description:
+ * This function pushes the given integer value onto the stack.
+ * If the stack is full, it displays an error message and terminates the program.
  */
-int main(int argc, char *argv[])
+void push(int num)
 {
-	int num;
+    if (top == STACK_SIZE - 1)
+    {
+        fprintf(stderr, "Stack Overflow\n");
+        exit(EXIT_FAILURE);
+    }
 
-	if (argc != 2)
-	{
-		printf("Usage: %s <integer>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Convert the command-line argument to an integer */
-	num = atoi(argv[1]);
-
-	if (num == 0 && argv[1][0] != '0')
-	{
-		/* Conversion failed, input was not an integer */
-		printf("L1: usage: push integer\n");
-		exit(EXIT_FAILURE);
-	}
-
-	/* Push the integer value onto the stack (do further processing here) */
-	printf("Pushed onto stack: %d\n", num);
-
-	return (0);
+    stack[++top] = num;
 }
