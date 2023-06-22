20cb31e01f8c7dfe5af2e6b26ecc0bdd864317c2
mburudev <ianmburu999@gmail.com>
2023-06-22 12:14:23 -0700

    Adhere to betty

--- monty.h
+++ monty.h
@@ -5,6 +5,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct stack_s
 {
 	int n;
