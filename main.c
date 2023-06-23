641830e68829e7af65eaf4707fe7a0b094e6136d
mburudev <ianmburu999@gmail.com>
2023-06-23 00:29:30 -0700

    check 4

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
     file = fopen(argv[1], "r");
     if (file == NULL)
     {
-        fprintf(stderr, "Error opening file.\n");
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
